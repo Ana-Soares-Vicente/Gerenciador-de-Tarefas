0f70dffafb5d655a177bd0e5d6e73ebd99f6176b
Ana-Soares-Vicente <a.vicente@catolicasc.edu.br>
2026-04-28 15:25:15 -0300

    feat(addTask): ele adiciona titulo e descricao

--- src/App.jsx
+++ src/App.jsx
@@ -23,15 +23,44 @@ function App() {
   },
 ]);
 
+  function onTaskClick(taskId) {
+   const newTasks = tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, isCompleted: !task.isCompleted };
+        }
+
+        return task;
+  });
+  setTasks(newTasks);
+  }
+
+function onDeleteTaskClick(taskId) {
+  const newTasks = tasks.filter((task) => task.id !== taskId)
+  setTasks(newTasks);
+}
+
+function onAddTaskSubmit (title, description) {
+  const newTask = {
+    id: tasks.length + 1,
+    title,
+    description,
+    isCompleted: false,
+  };
+  setTasks([...tasks, newTask]);
+}
 
   return (
    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-    <div className="w-[500px]">
+    <div className="w-[500px] space-y-4">
       <h1 className="text-3xl font-bold text-center  text-white mb-6">
       Gerenciador de Tarefas
       </h1>
-      <AddTask />
-      <Tasks tasks={tasks} />
+      <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+      <Tasks 
+      tasks={tasks} 
+      onTaskClick={onTaskClick} 
+      onDeleteTaskClick={onDeleteTaskClick} 
+      />
       </div> 
    </div>
   );
